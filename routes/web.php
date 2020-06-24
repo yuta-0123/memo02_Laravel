6d16cf5d2bb06ae3158d6781f4b650f01c859466
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-24 11:46:57 +0900

    routes と view

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
 });
 
 Route::get('/archives/', function () {
-    return '記事一覧';
+    return view('archives.index');
 });
 
 Route::get('/archives/{category}/', function ($category) {
-    return $category.'の一覧';
+    return view('archives.category',['category'=>$category]);
 });
